7e2d0966ecb9d3c91693c08b2a6eabfddd2c05d6
mariuscrsn <mariuscrsn@gmail.com>
2018-12-05 18:44:06 +0100

    Terminado script para sginar puertos a segmentos

--- configurador.py
+++ configurador.py
@@ -1,6 +1,7 @@
 import json
 import argparse
 import socket
+import subprocess
 
 def validate_port(port):
     return port >= 1 and port <= 24
@@ -20,60 +21,76 @@ def validate_ip(ip):
         return False
 
 def validate_input(json_object):
-    """ Devuelve true si json_object pasalos requisitos necesarios para ser un fichero válido
-        Para ver el formato de entrada válido véase format.json 
-        y para ver un ejemplo véase example.json
+    """ Devuelve true si json_object pasalos requisitos necesarios para ser un fichero valido
+        Para ver el formato de entrada valido vease format.json
+        y para ver un ejemplo vease example.json
     """
     try:
         if type(json_object) is not list:
-            print("json_object")
             return False
         for machine_config in json_object:
             if (type(machine_config["ip"]) is not str) or not validate_ip(machine_config["ip"]):
-                print("ip")
                 return False
             if type(machine_config["community"]) is not str:
-                print("community")
                 return False
             if type(machine_config["config"]) is not list:
-                print("config")
                 return False
             for actual_config in machine_config["config"]:
                 if (type(actual_config["segment"]) is not int) or not validate_segment(actual_config["segment"]):
-                    print("segment")
                     return False
                 if type(actual_config["ports"]) is not list:
-                    print("ports")
                     return False
                 for actual_port in actual_config["ports"]:
                     if (type(actual_port) is not int) or not validate_port(actual_port):
-                        print("ports2")
                         return False
     except KeyError as ke:
-        # Formato incorrecto debido a que algún campo no existe
-        print("ke")
+        # Formato incorrecto debido a que algun campo no existe
         return False
-    # Todos los campos existen y están bien
+    # Todos los campos existen y estan bien
     return True
 
 def config_hubs(json_object):
     ''' 
-        El fichero de entrada está validado y es correcto, por lo que se puede proceder
+        El fichero de entrada esta validado y es correcto, por lo que se puede proceder
         a configurar los hubs
     '''
-    # TODO: Implementar esta función
-    # Para implementarla mirar https://docs.python.org/3/library/subprocess.html
-    # Subprocess vale para lanzar comando de shell
-    pass
+
+    for machine_config in json_object:
+        ip = machine_config["ip"]
+        community = machine_config["community"]
+        snmp_res = subprocess.run(["snmpwalk", "-v", "1", "-c", community, ip, ".1.3.6.1.4.1.43.10.26.1.1.1.5"],
+                       capture_output=True)
+        if snmp_res.returncode != 0:
+            print("Fallo al establecer la conexion con el hub")
+            exit(1)
+        print ("==== Configurando el hub con ip: ", ip, "====")
+        for actual_config in machine_config["config"]:
+            segment = actual_config["segment"]
+            snmp_res = subprocess.run(
+                ["snmpget", "-v", "1", "-c", community, ip, ".1.3.6.1.4.1.43.10.26.1.1.1.5.1." + str(1000 + segment)],
+                capture_output=True)
+            if snmp_res.returncode != 0:
+                print("Fallo al obtener el valor del segmento")
+                exit(1)
+            seg_valor = int(snmp_res.stdout.split()[-1])
+            print("-- Añadiendo puertos al segmento:", segment, "--")
+            for port in actual_config["ports"]:
+                snmp_res = subprocess.run(
+                    ["snmpset", "-v", "1", "-c", community, ip,
+                     ".1.3.6.1.4.1.43.10.26.1.1.1.5.1." + str(port), "i", str(seg_valor)],
+                    capture_output=True)
+                if snmp_res.returncode != 0:
+                    print("Fallo al asignar el puerto al segmento")
+                    exit(1)
+                print("Puerto:", port)
 
 def main():
     # Parseador de argumentos
     parser = argparse.ArgumentParser(description='Configura los diferentes segmentos en los routers pasados')
     parser.add_argument('fichero', metavar='file', type=str,
-                        help='fichero de configuración a usar')
+                        help='fichero de configuracion a usar')
     args = parser.parse_args()
-    print(args.fichero)
-    try:    
+    try:
         with open(args.fichero, "r") as read_file:
             try:
                 data = json.load(read_file)
